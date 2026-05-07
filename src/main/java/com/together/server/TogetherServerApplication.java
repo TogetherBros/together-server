35f73680f401374fe62a9034bd380b79dc4c9a07
nuza <wjdansrud092208@gmail.com>
2026-05-07 11:17:08 +0900

    setting

---
+++ src/main/java/com/together/server/TogetherServerApplication.java
@@ -0,0 +1,13 @@
+package com.together.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TogetherServerApplication.class, args);
+  }
+
+}
